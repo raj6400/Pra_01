707ad0056aa1ba9e1429e3a6df3df361fe6dd608
RAJ <raj@example.com>
2026-07-17 12:34:45 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace StudentAdmissionSystem
+{
+    class Student
+    {
+        public int AdmissionNo;
+        public string Name;
+        public string Course;
+        public int Semester;
+        public double Fees;
+
+        public Student(int admissionNo, string name, string course, int semester, double fees)
+        {
+            AdmissionNo = admissionNo;
+            Name = name;
+            Course = course;
+            Semester = semester;
+            Fees = fees;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("\n-----------------------------------------------");
+            Console.WriteLine("         STUDENT ADMISSION DETAILS");
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.WriteLine("Admission Number : " + AdmissionNo);
+            Console.WriteLine("Student Name     : " + Name);
+            Console.WriteLine("Course           : " + Course);
+            Console.WriteLine("Semester         : " + Semester);
+            Console.WriteLine("Admission Fees   : " + Fees);
+
+           
+
+            Console.WriteLine("-----------------------------------------------");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------------------------------------");
+            Console.WriteLine("         STUDENT ADMISSION SYSTEM");
+            Console.WriteLine("-----------------------------------------------");
+
+            Console.Write("Enter Admission Number : ");
+            int admissionNo = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Student Name     : ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Course           : ");
+            string course = Console.ReadLine();
+
+            Console.Write("Enter Semester         : ");
+            int semester = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Admission Fees   : ");
+            double fees = Convert.ToDouble(Console.ReadLine());
+
+            Student s = new Student(admissionNo, name, course, semester, fees);
+
+            Console.WriteLine("\nStudent Object Created Successfully.");
+
+            s.Display();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+<img width="938" height="409" alt="image" src="https://github.com/user-attachments/assets/5e993d3b-f5cd-4f55-ae42-593c8c620d4a" />
+<img width="513" height="203" alt="image" src="https://github.com/user-attachments/assets/d9224b27-d3fc-4882-a29b-e38b17ee57e3" />
+<img width="816" height="504" alt="image" src="https://github.com/user-attachments/assets/4ce8fe82-22e4-456e-bdcc-517a0ba9cf4c" />
